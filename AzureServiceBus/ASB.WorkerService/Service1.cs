a4cb70f2725f06ad0cf99fbfa2749f8db9e71697
Mehrdad Kamelzadeh <mehrdad.kamelzadeh@gmail.com>
2019-12-17 21:56:17 -0500

    Added worker service

--- AzureServiceBus/ASB.WorkerService/Service1.cs
+++ AzureServiceBus/ASB.WorkerService/Service1.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.IO;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ASB.WorkerService
 {
     public partial class Service1 : ServiceBase
     {
+        static QueueClient queueClient;
+        static string sbConnectionString =
+            "Endpoint=sb://mehrdadplayground.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Kw7zOb5baMy64uFFUaMUg0sY4kmSWDaTnuK799hiZHw=";
+        static string sbQueueName = "eventqueue";
         public Service1()
         {
             InitializeComponent();
@@ -19,11 +21,75 @@ namespace ASB.WorkerService
 
         protected override void OnStart(string[] args)
         {
+            WriteToFile("Service is started at " + DateTime.Now);
+            RegisterHandler();
+        }
+        public async void RegisterHandler()
+        {
+            try
+            {
+                WriteToFile("Handlers started " + DateTime.Now);
 
+                queueClient = new QueueClient(sbConnectionString, sbQueueName);
+
+                var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+                {
+                    MaxConcurrentCalls = 1,
+                    AutoComplete = false
+                };
+
+                queueClient.RegisterMessageHandler(ReceiveMessagesAsync, messageHandlerOptions);
+            }
+            catch (Exception ex)
+            {
+                WriteToFile($"{ex.Message}  {DateTime.Now}");
+            }
+            finally
+            {
+                await queueClient.CloseAsync();
+            }
         }
 
+        private async Task ReceiveMessagesAsync(Message message, CancellationToken token)
+        {
+            WriteToFile($"Received message: {Encoding.UTF8.GetString(message.Body)}");
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            WriteToFile(exceptionReceivedEventArgs.Exception.Message);
+            return Task.CompletedTask;
+        }
         protected override void OnStop()
         {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!System.IO.Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.   
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
         }
     }
 }
---
+++ AzureServiceBus/ASB.WorkerServiceConsole/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ASB.WorkerServiceConsole
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            RegisterHandler();
+            Console.ReadLine();
+        }
+
+
+
+        static QueueClient queueClient;
+        static string sbConnectionString =
+            "Endpoint=sb://mehrdadplayground.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Kw7zOb5baMy64uFFUaMUg0sY4kmSWDaTnuK799hiZHw=";
+        static string sbQueueName = "eventqueue";
+
+        public static async void RegisterHandler()
+        {
+            try
+            {
+                queueClient = new QueueClient(sbConnectionString, sbQueueName);
+
+                var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+                {
+                    MaxConcurrentCalls = 1,
+                    AutoComplete = false
+                };
+
+                queueClient.RegisterMessageHandler(ReceiveMessagesAsync, messageHandlerOptions);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}  {DateTime.Now}");
+            }
+
+        }
+
+        private static async Task ReceiveMessagesAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message: {Encoding.UTF8.GetString(message.Body)}");
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine(exceptionReceivedEventArgs.Exception.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
